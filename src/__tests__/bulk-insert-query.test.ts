e5c46000116706058e06b7084c178b82113be58e
nirmal-ps <nirmal.ps@clairvolex.com>
2019-02-06 14:33:55 +0530

    update new version

--- src/__tests__/bulk-insert-query.test.ts
+++ src/__tests__/bulk-insert-query.test.ts
@@ -1,6 +1,6 @@
 import { GetBulkInsertQuery } from '../index';
 
 test('GetBulkInsertQuery TEST', () => {
-    const expectedOutput = "INSERT INTO table_name (name,age) values ( 'test_name', 26)"
-    expect(GetBulkInsertQuery('table_name' , ['name' , 'age'] , [{name: 'test_name' , age: 26}])).toBe(expectedOutput);
-  });
+  const expectedOutput = "INSERT INTO table_name (name,age) values ( 'test_name', 26)";
+  expect(GetBulkInsertQuery('table_name', ['name', 'age'], [{ name: 'test_name', age: 26 }])).toBe(expectedOutput);
+});
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ export const GetBulkInsertQuery = (tableName: string, columns: string[], values:
   const validValueObject = values.map((value: any) => {
     let queryString = `(`;
     for (let i = 0; i < validColumns.length; ++i) {
-      if (typeof value[validColumns[i]] === "number") {
+      if (typeof value[validColumns[i]] === 'number') {
         queryString = `${queryString} ${value[validColumns[i]]}${i !== validColumns.length - 1 ? ',' : ''}`;
-      } else if (typeof value[validColumns[i]] === "string") {
+      } else if (typeof value[validColumns[i]] === 'string') {
         queryString = `${queryString} '${value[validColumns[i]]}'${i !== validColumns.length - 1 ? ',' : ''}`;
-      } 
-      
+      }
     }
     return `${queryString})`;
   });
