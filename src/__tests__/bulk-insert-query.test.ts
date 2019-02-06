738139aca6450583c53621409e726efe9df5034e
nirmal-ps <nirmal.ps@clairvolex.com>
2019-02-06 15:55:58 +0530

    add packge update and test case fixes

--- src/__tests__/bulk-insert-query.test.ts
+++ src/__tests__/bulk-insert-query.test.ts
@@ -6,6 +6,6 @@ test('GetBulkInsertQuery TEST', () => {
 });
 
 test('GetBulkInsertQuery TEST CITY', () => {
-    const expectedOutput = "INSERT INTO table_name (name,age) values ('test_name',26),('test_name',26)";
+    const expectedOutput = "INSERT INTO test_table (id,city) values (1,'Delhi'),(2,'Bangalore')";
     expect(GetBulkInsertQuery('test_table', ['id', 'city'], [{ id: 1, city: 'Delhi' } , { id: 2 , city: 'Bangalore' }])).toBe(expectedOutput);
   });
